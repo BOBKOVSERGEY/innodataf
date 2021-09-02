27da2e741a4177afc2b948366f81a7fc6cbe3bad
BOBKOVSERGEY <sergey_bobkov@inbox.ru>
2021-09-03 00:58:10 +0300

    add form

--- src/js/main.js
+++ src/js/main.js
@@ -216,6 +216,7 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     });
     const body = document.querySelector('body');
+
     if(body) {
         body.classList.remove('preload');
     }
@@ -307,7 +308,120 @@ document.addEventListener('DOMContentLoaded', () => {
         })
     }
     /**end menu**/
-    
+
+    /** modal **/
+
+    const popupLinks = document.querySelectorAll('.popup-link');
+    const lockPadding = document.querySelectorAll('.lock-padding');
+
+    body.classList.remove('preload');
+
+    let unlock = true;
+
+    const timeout = 800;
+
+    if (popupLinks.length > 0) {
+        for (let index = 0; index < popupLinks.length; index++) {
+            const popupLink = popupLinks[index];
+            popupLink.addEventListener('click', function (e) {
+                const popupName = popupLink.getAttribute('data-modal').replace('#', '');
+                console.log(popupName);
+                const currentPopup = document.getElementById(popupName);
+                popupOpen(currentPopup);
+                e.preventDefault();
+            })
+        }
+    }
+
+    const popupCloseIcon = document.querySelectorAll('.popup__close-icon');
+    if (popupCloseIcon.length > 0) {
+        for (let index = 0; index < popupCloseIcon.length; index++) {
+            const el = popupCloseIcon[index];
+            el.addEventListener('click', function (e) {
+                popupClose(el.closest('.popup'));
+                e.preventDefault();
+            })
+        }
+    }
+
+    function popupOpen(currentPopup) {
+        if (currentPopup && unlock) {
+            const popupActive = document.querySelector('.popup.popup-open');
+            if (popupActive) {
+                popupClose(popupActive, false);
+            } else {
+                bodyLock();
+            }
+            currentPopup.classList.add('popup-open');
+            currentPopup.addEventListener('click', function (e) {
+                if (!e.target.closest('.popup__content')) {
+                    popupClose(e.target.closest('.popup'))
+                }
+            })
+        }
+    }
+
+    function popupClose(popupActive, doUnlock = true) {
+        if (unlock) {
+            popupActive.classList.remove('popup-open');
+            if (doUnlock) {
+                bodyUnLock();
+            }
+        }
+    }
+
+    function bodyLock() {
+        const lockPaddingValue = window.innerWidth - document.querySelector('.layout').offsetWidth + 'px';
+        //console.log(document.querySelector('.wrapper').offsetWidth);
+        //console.log(document.querySelector('body').offsetWidth);
+        if (lockPadding.length > 0) {
+            for (let index = 0; index < lockPadding.length; index++) {
+                const el = lockPadding[index];
+                el.style.paddingRight = lockPaddingValue;
+            }
+        }
+
+
+        body.style.paddingRight = lockPaddingValue;
+        body.classList.add('lock');
+
+        unlock = false;
+
+        setTimeout(function () {
+            unlock = true;
+        }, timeout)
+    }
+
+    function bodyUnLock() {
+        setTimeout(function () {
+            if (lockPadding.length > 0 ) {
+                for (let index = 0; index < lockPadding.length; index++) {
+                    const el = lockPadding[index];
+                    el.style.paddingRight = '0px';
+                }
+            }
+
+            body.style.paddingRight = '0px';
+            body.classList.remove('lock');
+        }, timeout);
+
+        unlock = false;
+
+        setTimeout(function () {
+            unlock = true;
+        }, timeout)
+    }
+
+    document.addEventListener('keydown', function (e) {
+        if (e.which === 27){
+            const popupActive = document.querySelector('.popup.popup-open');
+            if (popupActive) {
+                popupClose(popupActive);
+            }
+
+        }
+    });
+    /** end modal**/
     
     
 });
